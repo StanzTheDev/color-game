d8c8fff51617d22392af31b8d2c82008e235a526
StanzTheDev <135062697+StanzTheDev@users.noreply.github.com>
2025-04-01 20:07:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Easy from './easy.jsx';
 import Hard from './hard.jsx';
-import Medi from './assets/woman-meditation-lotus-pose-icon-isolated.png'
+import Medi from './assets/woman-meditation-lotus-pose-icon-isolated.png';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
 
 function App() {
     useEffect(() => {
       AOS.init({ duration: 1200 });
     }, []);
+  
   return (
-
     <BrowserRouter> 
       <Routes>
         <Route
@@ -23,8 +22,8 @@ function App() {
               <h1 id='h1-a'>Welcome To My Eye Color Game</h1>
               <img className='medi-img' src={Medi} alt="" />
               <div className='buttons'>
-              <Link to="/easy"><button className="b3n">Easy</button></Link>
-              <Link to="/hard"><button className="b3n">Hard</button></Link>
+                <Link to="/easy"><button className="b3n">Easy</button></Link>
+                <Link to="/hard"><button className="b3n">Hard</button></Link>
               </div>
             </div>
           }
@@ -33,5 +32,7 @@ function App() {
         <Route path="/hard" element={<Hard />} />
       </Routes>
     </BrowserRouter>
-      )
+  );
+}
+
 export default App;
